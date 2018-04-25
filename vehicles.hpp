4a9a05d4546df630517d780b8bb4ba0a72df4ba3
inpgczwartki16 <38723961+inpgczwartki16@users.noreply.github.com>
2018-04-25 14:33:09 +0200

    Update vehicles.hpp

--- vehicles.hpp
+++ vehicles.hpp
@@ -78,3 +78,22 @@ std::string to_string(const Driver& owner);
 void assign_vehicle_to_driver(std::vector<std::unique_ptr<Vehicle>>& vehicles, Driver& owner);
 
 #endif /* VEHICLES_HPP_ */
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+string random(int len)
+{
+	string a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	string r;
+	srand(time(NULL));
+	for(int i = 0; i < len; i++) r.push_back(a.at(size_t(rand() % 62)));
+	return r;
+}
+int main()
+{
+	cout << random(32) << endl;
+	cin.get();
+	return 0;
+}
