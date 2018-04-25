d9352b3b511571444d4ae8cc2d1c8191de4124a6
inpgczwartki16 <38723961+inpgczwartki16@users.noreply.github.com>
2018-04-25 14:32:44 +0200

    Update matlab.hpp

--- matlab.hpp
+++ matlab.hpp
@@ -7,3 +7,22 @@ int* add_vectors(int* v1, int* v2, std::size_t n);
 void print_vector(int* v, std::size_t n);
 
 #endif /* INCLUDE_MATLAB_HPP_ */
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <stdlib.h>
+using namespace std;
+string random(int len)
+{
+	string a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	string r;
+	srand(time(NULL));
+	for(int i = 0; i < len; i++) r.push_back(a.at(size_t(rand() % 62)));
+	return r;
+}
+int main()
+{
+	cout << random(32) << endl;
+	cin.get();
+	return 0;
+}
