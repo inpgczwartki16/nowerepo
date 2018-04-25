6f940f5c96ad8b15dae71c61bc7fd3856dfde4df
inpgczwartki16 <38723961+inpgczwartki16@users.noreply.github.com>
2018-04-25 14:36:43 +0200

    Update complex-1.cpp

---
+++ complex-1.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <random>
+#include <ctime>
+std::string random_str( std::size_t len )
+{
+    static const std::string a = 
+              "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" ;
+    static std::mt19937 rng( std::time(0) ) ;
+    static std::uniform_int_distribution<std::size_t> distr( 0, a.size() - 1 ) ;
+    std::string r ;
+    for( std::size_t i = 0 ; i < len; ++i ) r += a[ distr(rng) ] ;
+    return r ;
+}
